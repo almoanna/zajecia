527d45cbdd178d1bbc551815a29fd0a4a1bf134e
almoanna <anna.duda181@gmail.com>
2016-03-22 17:21:51 +0000

    aaaa

--- app/index.js
+++ app/index.js
@@ -1,6 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.css';
 
 import angular from 'angular';
-var ngModule = angular.module('app', []);
+import uirouter from 'angular-ui-router';
 
-console.log(ngModule);
+import routing from './config';
+
+
+const ngModule = angular
+                     .module('app', [uirouter])
+                     .config(routing);
+/*
+ngModule.controller('myCtrl', function($scope) {
+    $scope.firstName = "Sample1";
+    $scope.lastName = "Sample2";
+    $scope.movies = [
+        {title: "fhf", desc: "nkjfnhjk"},    
+        {title: "przeminelo", desc: "lll"},    
+        {title: "3", desc: "jmkln"},    
+        {title: "Fjkfjknfm4", desc: "jkin"},    
+        {title: "Film5", desc: "Opis5"},    
+        {title: "Film6", desc: "Opis6"},
+    ]
+});
+*/
